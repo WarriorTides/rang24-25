82baa55c5009226579bf3e10fac6ea7899ecf789
amaheshwari01 <68670569+amaheshwari01@users.noreply.github.com>
2025-01-22 17:03:31 -0800

    sockettest

--- control-server/sockettest.py
+++ control-server/sockettest.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import socket
-=======
->>>>>>> 9faadee343b44c68dbafd8da5bde588ed69ce9df
 import time
 
 import socketio
@@ -12,11 +9,6 @@ sio = socketio.Client()
 
 @sio.event
 def connect():
-<<<<<<< HEAD
-=======
-    sio.emit("joystick", "jsdoiafjoisj")
-
->>>>>>> 9faadee343b44c68dbafd8da5bde588ed69ce9df
     print(" connected!")
 
 
@@ -26,7 +18,6 @@ def disconnect():
 
 
 def runStuff():
-<<<<<<< HEAD
     sio.connect("http://Anyas-MacBook-Pro.local:5001", transports=["websocket"])
     time.sleep(0.2)
 
@@ -47,22 +38,3 @@ def runStuff():
 
 
 runStuff()
-=======
-    sio.connect("http://192.168.1.164:5001", transports=["websocket"])
-    time.sleep(0.2)
-
-    # try:
-    #     while True:
-
-
-    #         time.sleep(0.1)
-
-    # except KeyboardInterrupt:
-    #     print("Closing socket")
-    #     sio.disconnect()
-
-    # finally:
-    #     print("Closing socket")
-    #     sio.disconnect()
-runStuff()
->>>>>>> 9faadee343b44c68dbafd8da5bde588ed69ce9df
